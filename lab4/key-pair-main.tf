5f7df674d280fe6b3f40e889dcb05563054935a1
stepandobr <dobryanskyi.stepan@student.uzhnu.edu.ua>
2022-11-08 15:05:50 +0200

    lab 4

---
+++ lab4/key-pair-main.tf
@@ -0,0 +1,16 @@
+resource "tls_private_key" "key_pair" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create the Key Pair
+resource "aws_key_pair" "key_pair" {
+  key_name   = "${lower("lab4")}-${lower("lab4")}-linux-${lower(var.aws_region)}"
+  public_key = tls_private_key.key_pair.public_key_openssh
+}
+
+# Save file
+resource "local_file" "ssh_key" {
+  filename = "${aws_key_pair.key_pair.key_name}.pem"
+  content  = tls_private_key.key_pair.private_key_pem
+}
---
+++ lab4/provider-variables.tf
@@ -0,0 +1,12 @@
+variable "aws_access_key" {
+  type = string
+  default = "AKIA6FSAQZKTSY7HMS4T"
+}
+variable "aws_secret_key" {
+  type = string
+  default = "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"
+}
+variable "aws_region" {
+  type = string
+  default = "eu-central-1"
+}
